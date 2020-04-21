838222c68aed1d32b3a47f92c747ac42879ee784
h0garr <musicki.aleksandar998@gmail.com>
2020-04-21 17:55:18 +0200

    Update

--- middlewares/validation/schemas/users.js
+++ middlewares/validation/schemas/users.js
@@ -6,7 +6,9 @@ const addUserSchema = Joi.object({
   nickname: Joi.string().min(5).required(),
   age: Joi.number().required(),
   email: Joi.string().required(),
-  password: Joi.string().required(),
+  password: Joi.string()
+    .regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,100}$/)
+    .required(),
 });
 
 const addWishlistSchema = Joi.object({
@@ -14,5 +16,9 @@ const addWishlistSchema = Joi.object({
     .regex(/^[0-9a-f]{24}$/)
     .required(),
 });
+const validateLogin = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+});
 
-module.exports = { addUserSchema, addWishlistSchema };
+module.exports = { addUserSchema, addWishlistSchema, validateLogin };
--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 const Login = require("../controllers/login");
 
+const { validation } = require("../middlewares/validation/validation");
+const { validateLogin } = require("../middlewares/validation/schemas/users");
+
 const { logUser } = Login;
 
-router.route("/").post(logUser);
+router.route("/").post(validation(validateLogin), logUser);
 
 module.exports = router;
